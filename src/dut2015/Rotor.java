8a7cd277b72b5ccf2f28af3158c5c3cfc6cb04ce
ludovic landschoot <ludovic.landschoot@info0610.iut-lens.univ-artois.fr>
2015-03-13 17:47:15 +0100

    Création classe rotor.

---
+++ src/dut2015/Rotor.java
@@ -0,0 +1,41 @@
+package dut2015;
+
+public class Rotor implements Enigma {
+
+	@Override
+	public void setRotor(Position pos, int rotorNumber) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getRotor(Position pos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void moveRotorToLetter(Position pos, char letter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public char getRotorLetter(Position pos) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getCurrentLetters() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char encode(char c) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
